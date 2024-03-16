fb1be35872a8a1325ff533acc05fdb989079875f
kovacsdaniel420 <159771306+kovacsdaniel420@users.noreply.github.com>
2024-03-16 14:36:04 +0100

    Add files via upload

---
+++ readfile.py
@@ -0,0 +1,16 @@
+
+def LoadPosoitonsFromFile():
+    gyongyok = open("gyongyok.txt") # gyongyok.txt megnyitása
+    sorok = gyongyok.readlines() # gyongyok.txt kiolvasása
+
+    gyongyok.close() # gyongyok.txt bezárása
+    sorok.pop(0) # Legelső sor eltávolítása (x;y;z;e; sor)
+
+    positions = [] # Rendezett pozicíók listája
+
+    for i in range(len(sorok)):
+        sor = sorok[i].split(";") # Elemek poziciójának és értéküknek eltárolása egy listába -> ["x", "y", "z", "e"]
+        sor = sor[:-1] # "\n" elem eltávolítsa a listából
+        positions.append(sor) # Rendezett poziciók hozzáadása a listához
+
+    return positions # Rendezett pozició lista visszaadása
