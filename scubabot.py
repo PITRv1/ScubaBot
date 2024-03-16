376dc94cc7a0ee0692431a5027a3fb875b39bc46
kovacsdaniel420 <159771306+kovacsdaniel420@users.noreply.github.com>
2024-03-16 14:55:33 +0100

    Add files via upload

--- scubabot.py
+++ scubabot.py
@@ -1,18 +1,9 @@
 from ursina import *
+from readfile import LoadPositionsFromFile
 
 app = Ursina()
-file = open('gyongyok.txt', 'r')
-read = file.readlines()
-modified = []
 
-for line in read:
-  if line[-1] =='\n':
-    modified.append(line[:-1])
-  else:
-    modified.append(line)
-
-modified.pop(0)
-modified = [line.replace(';', ',') for line in modified]
+poziciok = LoadPositionsFromFile()
 
 diveBot = Entity(model="sphere", color=rgb(200,200,0), scale=1, collider="sphere")
 diveBot.position = Vec3(0,0,0)
@@ -24,12 +15,12 @@ water.alpha = .1
 globalValue = 0
 
 def point(x,y,z,value):
-  point = Entity(model="cube", color=rgb(200,0,0), scale=value/3, collider="sphere")
-  point.position = Vec3(x,y,z)
+  point = Entity(model="cube", color=rgb(200,0,0), scale=int(value)/3, collider="sphere")
+  point.position = Vec3(int(x),-int(y),int(z))
   point.alpha = .9
 
-point(40, -50, 10, 5)
-point(80, -30, 40, 10)
+for list in poziciok:
+  point(list[0], list[1], list[2], list[3])
 
 EditorCamera()
 
