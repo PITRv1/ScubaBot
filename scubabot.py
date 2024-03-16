2bd32eb80d0be5f78de0125d84d6b1f37335913a
kovacsdaniel420 <159771306+kovacsdaniel420@users.noreply.github.com>
2024-03-16 21:31:24 +0100

    Updates things

--- scubabot.py
+++ scubabot.py
@@ -18,9 +18,10 @@ water = Entity(model="cube", color=rgb(0,0,100), scale=100)
 water.position = Vec3(50,-50,50)
 water.alpha = .1
 
+#xd
 inRangePoints = []
 
-#Dunci xd
+# xd
 
 def point(x,y,z,value):
   point = Entity(model="cube", color=rgb(200,0,0), scale=int(value)/3, collider="cube", )
