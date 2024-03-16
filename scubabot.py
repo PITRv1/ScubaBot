2730c8cdbf45d7a43ba0f589a8c3e37d6b3a7961
borokmate <borokmate@gmail.com>
2024-03-16 20:53:05 +0100

    Bugig
    Alma

--- scubabot.py
+++ scubabot.py
@@ -12,6 +12,8 @@ water = Entity(model="cube", color=rgb(0,0,100), scale=100)
 water.position = Vec3(50,-50,50)
 water.alpha = .1
 
+print("alma")
+
 def point(x,y,z,value):
   point = Entity(model="cube", color=rgb(200,0,0), scale=int(value)/3, collider="cube")
   point.position = Vec3(int(x),-int(y),int(z))
